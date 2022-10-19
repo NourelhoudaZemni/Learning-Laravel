9518b1427d179edcd4580f4e5ca1abc99890986c
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-10-19 20:29:19 +0100

    Dashboard update

---
+++ resources/views/home/userpage.blade.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+   <head> 
+      <meta charset="utf-8"/>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta name="keywords" content="" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <link rel="shortcut icon" href="images/favicon.png" type="">
+      <title>Famms - Fashion HTML Template</title> 
+      <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" /> 
+      <link href="home/css/font-awesome.min.css" rel="stylesheet" /> 
+      <link href="home/css/style.css" rel="stylesheet" /> 
+      <link href="home/css/responsive.css" rel="stylesheet" />
+   </head>
+   <body> 
+@include('home.header') 
+         
+@include('home.slider')   
+      
+@include('home.why')  
+
+      @include('home.new_arrival')  
+      
+      @include('home.product')  
+  
+      @include('home.client') 
+      @include('home.client')  
+
+      @include('home.footer')  
+      <div class="cpy_">
+         <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
+         
+            Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+         
+         </p>
+      </div>
+      <!-- jQery -->
+      <script src="home/js/jquery-3.4.1.min.js"></script>
+      <!-- popper js -->
+      <script src="home/popper.min.js"></script>
+      <!-- bootstrap js -->
+      <script src="home/bootstrap.js"></script>
+      <!-- custom js -->
+      <script src="home/custom.js"></script>
+   </body>
+</html>
