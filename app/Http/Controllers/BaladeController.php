03526be6f5c5d02d2dfd9b60c0e1f6e3f57d24d4
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-10-19 22:10:13 +0100

    Add balade controller

---
+++ app/Http/Controllers/BaladeController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Balade;
+class BaladeController extends Controller
+{
+    public function index()
+    {
+        $balades = Balade::all();
+        return view ('admin.balades.index')->with('balades', $balades);
+    }
+    
+    public function create()
+    {
+        return view('admin.balades.create');
+    }
+  
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        Balade::create($input);
+        return redirect('balade')->with('flash_message', 'balade Addedd!');  
+    }
+    
+    public function show($id)
+    {
+        $balade = Balade::find($id);
+        return view('admin.balades.show')->with('balades', $balade);
+    }
+    
+    public function edit($id)
+    {
+        $balade = Balade::find($id);
+        return view('admin.balades.edit')->with('balades', $balade);
+    }
+  
+    public function update(Request $request, $id)
+    {
+        $balade = Balade::find($id);
+        $input = $request->all();
+        $balade->update($input);
+        return redirect('balade')->with('flash_message', 'balade Updated!');  
+    }
+  
+    public function destroy($id)
+    {
+        Balade::destroy($id);
+        return redirect('balade')->with('flash_message', 'balade deleted!');  
+    }
+}
